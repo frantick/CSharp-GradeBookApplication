b639d90f2aa1b98d9d06c7d5b2b1a65d1885a7ed
Frantick <31290050+frantick@users.noreply.github.com>
2019-03-30 22:38:10 +0100

    Completed the module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -44,6 +44,7 @@ namespace GradeBook.GradeBooks
                 Console.Write("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
                 return;
             }
+
             base.CalculateStudentStatistics(name);
         }
     }
